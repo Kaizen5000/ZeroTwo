ccb3dd959151e6383efbfb7d01ca28e48908f3af
Nelson Tang <nelson.tang1@hotmail.com>
2018-05-31 13:08:26 +1000

    fixed help command to work with new structure

--- commands/help.js
+++ commands/help.js
@@ -16,7 +16,7 @@ exports.execute = async (message, args) =>
 	if (!args.length) // Display all commands
 	{
 		data.push("Here's a list of all my commands:");
-		data.push(commands.map(command => command.name).join(", "));
+		data.push(commands.map(command => command.data.name).join(", "));
 		data.push("\nYou can send `${prefix}help [command name]` to get info on a specific command!");
 	}
 	else // Display info about a specific command
