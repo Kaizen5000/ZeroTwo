f82ef2f9b8f19557866a49f603558b89b1eef8ab
Nelson Tang <nelson.tang1@hotmail.com>
2018-09-22 23:07:35 +1000

    fixed number limit and number range checks

--- commands/daily.js
+++ commands/daily.js
@@ -9,28 +9,33 @@ exports.data = {
 };
 exports.execute = async (client, message, args) =>
 {
+	let http = new XMLHttpRequest();
 	// Try to get a valid URL 20 times
-	for (let i = 0; i < 20; ++i)
+	for (let i = 0; i < 50; ++i)
 	{
 		// Random number between 100000 and 999999
-		const code = Math.floor(Math.random() * 999999) + 100000;
+		let code = Math.floor(Math.random() * 999999);
+   		code += (code < 100000) ? 100000 : 0;
 		// Format URL
 		const url = "https://nhentai.net/g/" + code + "/";
 		// Establish connection to website
-		const http = new XMLHttpRequest();
+		http = new XMLHttpRequest();
 		http.open("HEAD", url, false);
 		http.send();
 		// If not error code 404
-		if (http.status != 404)
+		// if (http.status != 404)
+    	if (http.status >= 200 && http.status <= 299)
 		{
 			// Send url and iteration number
 			await message.channel.send(url + " " + i);
-			break; // Exit loop
+     	 	return; // Finish command
 		}
 		else
 		{
+			console.log(code + " " + http.status + " " + i);
 			// Do next iteration
 			continue;
 		}
 	}
+	await message.channel.send("failed, I cri. status " + http.status);
 };
